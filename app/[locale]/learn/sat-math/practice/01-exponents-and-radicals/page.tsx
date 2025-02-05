c0d09ebab63d83e34894b8b315359e5463c0eefd
davidpangsw <davidpangsw@hotmail.com>
2025-02-04 23:29:55 -0800

    comments

--- app/[locale]/learn/sat-math/practice/01-exponents-and-radicals/page.tsx
+++ app/[locale]/learn/sat-math/practice/01-exponents-and-radicals/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 // TODO: Import a multiple choice generator
 // Implement the questions and answers
+// https://github.com/wingkwong/react-quiz-component
 
 const page = () => {
   return (
